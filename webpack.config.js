ae58425dc970d7f616c39351bc557ff022d29d57
Alfiki Diastama Afan Firdaus <alfiki.diastama@gmail.com>
2023-04-16 13:42:26 +0700

    Update bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
  
 module.exports = {
   entry: './src/app.js',
-  mode: 'development',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: "bundle.js"
